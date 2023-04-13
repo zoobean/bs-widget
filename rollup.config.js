9a3e621fe65c745623ba089751f4d61d82d561a6
Mike Kuzin <mike@kuzn.me>
2023-04-13 19:37:33 -0400

    Next attempt

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,13 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
-import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/main.js",
   output: [
     {
+      name: "BSWidget",
       file: "dist/bundle.js",
-      format: "cjs",
-    },
-    {
-      file: "dist/bundle.min.js",
       format: "iife",
-      name: "version",
-      plugins: [terser()],
     },
   ],
   plugins: [resolve(), babel({ babelHelpers: "bundled" })],
